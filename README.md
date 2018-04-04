eea7130a63b21e20e967dd03f55548e00ee94dfd
Maxim Shushakov <shushakov.maxim@gmail.com>
2018-04-04 13:12:11 +0700

    Init/Add Shapes Repo

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# shapes
+Shapes
+=======
+
+Based on these three points this app draws:
+- A parallelogram, having three of its corners in the points selected by the user.
+- A circle, with the same area and centre of mass as the parallelogram.
+
+Features:
+--------
+
+* ~100 lines of plain JS code
+* Mouse/Touch support
+* Multi-touch support
+
+![Example](http://image.ibb.co/hn7aux/Screen_Shot_2018_04_04_at_1_08_24_PM.png)
---
+++ app.js
@@ -0,0 +1,137 @@
+// Helpers
+class Helpers {
+	static calc4thPoint(points) {
+		return {
+			x: points[0].x + (points[2].x - points[1].x),
+			y: points[0].y + (points[2].y - points[1].y),
+		}
+	}
+	static calcCenterPoint(points) {
+		return {
+			x: points[0].x + (points[2].x - points[0].x) / 2,
+			y: points[0].y + (points[2].y - points[0].y) / 2,
+		}
+	}
+	static calcArea(points) {
+		const a = Math.sqrt(Math.pow(points[0].x- points[1].x, 2) + Math.pow(points[0].y - points[1].y, 2));
+		const b = Math.sqrt(Math.pow(points[0].x - points[3].x, 2) + Math.pow(points[0].y - points[3].y, 2));
+		const c = Math.sqrt(Math.pow(points[1].x - points[3].x, 2) + Math.pow(points[1].y - points[3].y, 2));
+		const p = (a + b + c) / 2;
+
+		return 2 * Math.sqrt(p * (p - a) * (p - b) * (p - c));
+	}
+	static calcRadius(points) {
+		return Math.sqrt(Helpers.calcArea(points) / Math.PI);
+	}
+}
+
+const create = (tag, params) => {
+	const elem = document.createElementNS('http://www.w3.org/2000/svg', tag);
+	if (params) Object.keys(params).forEach(key => elem.setAttribute(key, params[key]));
+	return elem;
+}
+
+const offset = (elem, x, y) => {
+	return {
+		x: x - elem.getBoundingClientRect().x,
+		y: y - elem.getBoundingClientRect().y,
+	}
+}
+
+// App
+const svg = document.querySelector('svg');
+const state = {
+	isDragging: false,
+	points: new Map(),
+	hints: new Map(),
+	touches: [],
+}
+const shapes = { // calculated shapes
+	polygon: create('polygon'),
+	circle: create('circle'),
+}
+
+svg.appendChild(shapes.polygon);
+svg.appendChild(shapes.circle);
+
+const hint = (elem, { x, y }, caption) => {
+	const hint = state.hints.has(elem) ? state.hints.get(elem) : create('text');
+	hint.textContent = (caption) ? `${caption}` :`${Math.round(x)}, ${Math.round(y)}`;
+	hint.setAttribute('x', x);
+	hint.setAttribute('y', y);
+	svg.appendChild(hint);
+	state.hints.set(elem, hint);
+}
+
+const update = () => {
+	state.points.forEach((position, element) => {
+		element.setAttribute('cx', position.x);
+		element.setAttribute('cy', position.y);
+		hint(element, position);
+	});
+
+	if (state.points.size === 3) {
+		const points = Array.from(state.points.values());
+		points.push(Helpers.calc4thPoint(points));
+		
+		const center = Helpers.calcCenterPoint(points);
+		const radius = Helpers.calcRadius(points);
+		const area = Helpers.calcArea(points);
+		
+		shapes.polygon.setAttribute("points", points.map((point) => point.x + ' ' + point.y).join(','));
+		shapes.circle.setAttribute("cx", center.x);
+		shapes.circle.setAttribute("cy", center.y);
+		shapes.circle.setAttribute("r", radius);
+		hint(shapes.circle, center, Math.round(area) + 'sq.pxs');
+	}
+	
+	if (state.isDragging) requestAnimationFrame(update);
+}
+
+// Events
+const dragStart = (e) => {
+	const touch = e.changedTouches && e.changedTouches[0] || e;
+	const identifier = touch.identifier || 0;
+	const { x, y } = offset(svg, touch.clientX, touch.clientY);
+	const point = state.points.has(touch.target) ? touch.target : create('circle', { cx: x, cy: y, r: 11 });
+
+	if (state.points.has(point) || state.points.size < 3) {
+		svg.appendChild(point); 
+		state.points.set(point, { x, y });
+		state.touches[identifier] = point;
+	}
+	else {
+		state.touches[identifier] = null;
+	}
+	
+	state.isDragging = true;
+
+	requestAnimationFrame(update);
+}
+
+const dragMove = (e) => {
+	if (!state.isDragging) return;
+	const touches = e.touches && Array.from(e.touches) || [ e ];
+	
+	touches.forEach(touch => { 
+		const identifier = touch.identifier || 0;
+		if (!state.touches[identifier]) return;
+		state.points.set(state.touches[identifier], offset(svg, touch.clientX, touch.clientY));
+	});
+}
+
+const dragStop = (e) => {
+	const identifier = e.changedTouches && e.changedTouches[0] || 0;
+	state.touches[identifier] = null;
+	if (!e.touches || !e.touches.length) state.isDragging = false;
+}
+
+svg.addEventListener('mousedown', dragStart);
+svg.addEventListener('touchstart', dragStart);
+
+svg.addEventListener('mousemove', dragMove);
+svg.addEventListener('touchmove', dragMove);
+
+svg.addEventListener('mouseup', dragStop);
+svg.addEventListener('touchend', dragStop);
+svg.addEventListener('mouseleave', dragStop);
